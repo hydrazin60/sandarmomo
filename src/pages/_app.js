8294a27b9ff42f74a65e811b2d902ff6079391d4
hydrazin60 <sureshkhdk45@gmail.com>
2024-09-20 14:09:02 +0545

    basic project setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import Home from "@/components/layouts/Home";
 import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <Home>
+        <Component {...pageProps} />
+      </Home>
+    </ThemeProvider>
+  );
 }
